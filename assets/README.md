c62d5c95138d6e778c3bb9f026cf8648434915ff
Dale Kittendorf <60675599+drkittendorf@users.noreply.github.com>
2020-05-16 19:43:41 -0500

    functioning. beginning final tweaks.

--- assets/README.md
+++ assets/README.md
@@ -1,51 +1,13 @@
 # 04CodeQuiz
-Homework assignment 4 
 
+Homework assignment 4
 
+When a user clicks on the link at https://drkittendorf.github.io/04CodeQuiz/
+they will be presented with a button to Begin Code Quiz.
+When the user clicks the button, a countdown timer will begin in the header,
+a question will then be presented in the box below the start button.
+When the user selects True or False, the next question appears in the text box.
+When an incorrect answer is selected 5 additional seconds are removed from the countdown timer.
+After the questions are complete or the time runs out,
+the user is prompted to enter their initials and it is saved with their score.
 
-
-
-Delete below here before submission
-
-# 04 Web APIs: Code Quiz
-
-As you proceed in your career as a web developer, you will probably be asked to complete a coding assessment, which is typically a combination of multiple-choice questions and interactive challenges. Build a timed code quiz with multiple-choice questions. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
-
-## User Story
-
-```
-AS A coding bootcamp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
-```
-
-## Acceptance Criteria
-
-```
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and score
-```
-
-The following animation demonstrates the application functionality:
-
-![code quiz](./Assets/04-web-apis-homework-demo.gif)
-
-### Review
-
-You are required to submit the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
--- script.js
+++ script.js
@@ -4,9 +4,27 @@ let buttonQuiz = document.getElementById("quizBtn");
 buttonQuiz.addEventListener("click", respondClick);
 let timerQuiz = document.getElementById("countdown");
 //question and answer variables
-const questArray = ["JavaScript was invented in 5 days. True or False?", "JavaScript is a cool language to learn. True or False?", "HTML stands for Hyper Text Made Up Language, True or False?", "Question", "another Question", "Yet another Question", "Last Question", "You answered all the questions!" ]
-const ansArray = ["False", "True", "False", "True", "False", "True", "False", "True"]
-let userAnswers = []
+const questArray = [
+  "JavaScript was invented in 5 days. True or False?",
+  "JavaScript is a cool language to learn. True or False?",
+  "HTML stands for Hyper Text Made Up Language, True or False?",
+  "Question",
+  "another Question",
+  "Yet another Question",
+  "Last Question",
+  "You answered all the questions!",
+];
+const ansArray = [
+  "False",
+  "True",
+  "False",
+  "True",
+  "False",
+  "True",
+  "False",
+  "True",
+];
+let userAnswers = [];
 let scoreCard = 0;
 
 let question = document.getElementById("quesTions");
@@ -15,89 +33,95 @@ let answerTrue = document.querySelector("#ansTrue");
 let answerFalse = document.querySelector("#ansFalse");
 
 // function below (started by begin code quiz) sets time, starts countdown timer, triggers Question function and Quiz End
+var timeInterval;
 
-function respondClick () {
-  buttonQuiz.disabled = "true"
-  console.log("clicked")  
+function respondClick() {
+  buttonQuiz.disabled = "true";
+  console.log("clicked");
   questionsQuiz(); // calls questions for user to answer
-  var timeInterval = setInterval(function() {
-  timerQuiz.textContent = timeLeft + " seconds remaining";
-  timeLeft--; // this counts the time down
+  timeInterval = setInterval(function () {
+    timerQuiz.textContent = timeLeft + " seconds remaining";
+    timeLeft--; // this counts the time down
     if (timeLeft <= 0) {
-    timerQuiz.textContent = "Out of Time!!!";
-    endQuiz();
-    clearInterval(timeInterval);
+      timerQuiz.textContent = "Out of Time!!!";
+      endQuiz();
     }
   }, 1000);
 }
 let questNum = 0;
 
-function questionsQuiz () {
+function questionsQuiz() {
   event.preventDefault();
-  if (questArray[questNum] != undefined) { // check here
-  question.innerHTML = questArray[questNum];   
-  console.log("questions happening"); 
-    } else {
-  console.log("undefined")
-   endQuiz()
-  return
+  if (questArray[questNum] != undefined) {
+    // check here
+    question.innerHTML = questArray[questNum];
+    console.log("questions happening");
+  } else {
+    endQuiz();
+    return;
   }
-}  
+}
 
 // below establish function to be called after user select an answer
-answerTrue.addEventListener("click", function() {
+answerTrue.addEventListener("click", function () {
   event.preventDefault();
   userAnswers.push("True");
-  answerTrue.disabled = true;//block clicks
+  answerTrue.disabled = true; //block clicks
   answerFalse.disabled = true;
   console.log(userAnswers, "user answer array");
-  
-  if (userAnswers[questNum]!==ansArray[questNum]) {  
-    console.log("answered incorrect")
-    timeLeft -=5;
-    answerTrue.disabled = false;//unblock clicks
+
+  if (userAnswers[questNum] !== ansArray[questNum]) {
+    console.log("answered incorrect");
+    timeLeft -= 5;
+    answerTrue.disabled = false; //unblock clicks
     answerFalse.disabled = false;
-    ++questNum
-  questionsQuiz ();
-     } else {
+    ++questNum;
+    questionsQuiz();
+  } else {
     ++scoreCard;
-    console.log(scoreCard)
-    answerTrue.disabled = false;//unblock clicks
+    console.log(scoreCard);
+    answerTrue.disabled = false; //unblock clicks
     answerFalse.disabled = false;
     console.log("answered correct");
-    ++questNum
-  questionsQuiz ();
-    } 
-})
+    ++questNum;
+    questionsQuiz();
+  }
+});
 
-answerFalse.addEventListener("click", function() {
+answerFalse.addEventListener("click", function () {
   event.preventDefault();
   userAnswers.push("False");
-  answerTrue.disabled = true;//block clicks
+  answerTrue.disabled = true; //block clicks
   answerFalse.disabled = true;
-  console.log(userAnswers,"user answer array");
-  
-  if (userAnswers[questNum]!==ansArray[questNum]) { 
-    console.log("answered incorrect")
-    timeLeft -=5;
-    answerTrue.disabled = false;//unblock clicks
+  console.log(userAnswers, "user answer array");
+
+  if (userAnswers[questNum] !== ansArray[questNum]) {
+    console.log("answered incorrect");
+    timeLeft -= 5;
+    answerTrue.disabled = false; //unblock clicks
     answerFalse.disabled = false;
-    ++questNum
-  questionsQuiz ();
-     } else {
+    ++questNum;
+    questionsQuiz();
+  } else {
     ++scoreCard;
-    console.log(scoreCard)
-    answerTrue.disabled = false;//unblock clicks
+    console.log(scoreCard);
+    answerTrue.disabled = false; //unblock clicks
     answerFalse.disabled = false;
     console.log("answered correct");
-    ++questNum
-  questionsQuiz ();
-    }   
-})        
-  
-  function endQuiz() {
+    ++questNum;
+    questionsQuiz();
+  }
+});
+
+function endQuiz() {
+  clearInterval(timeInterval);
   console.log("Your Quiz has Ended!");
   console.log(scoreCard);
-  prompt("Please enter your initials");
-  buttonQuiz.disabled = "false"
-  }
+  let name = prompt("Please enter your initials");
+  localStorage.setItem("Name", name);
+  localStorage.setItem("Score", scoreCard);
+  console.log(localStorage);
+  buttonQuiz.disabled = "false";
+  answerTrue.disabled = true; //block clicks
+  answerFalse.disabled = true;
+}
